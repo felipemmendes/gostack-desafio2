58783ad0ac7c47e2fdc0af4954beb39149fc06b6
felipemmendes <fmagliom@gmail.com>
2020-04-09 00:50:58 -0300

    Project CRUD, Likes

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get("/repositories", (request, response) => {
 app.post("/repositories", (request, response) => {
   const { title, url, techs } = request.body;
 
-  const newRepository = {
+  const repository = {
     id: uuid(),
     title,
     url,
@@ -25,33 +25,29 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  repositories.push(newRepository);
+  repositories.push(repository);
 
-  return response.json(newRepository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { title, url, techs } = request.body;
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(r => r.id === id);
+  let repository = repositories.find(r => r.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(400).json({ error: 'Repository not found.'})
   };
 
-  const currentRepository = repositories[repositoryIndex];
-  
-  const updatedRepository = {
-    ...currentRepository,
+  repository = {
+    ...repository,
     title,
     url,
     techs,
   };
 
-  repositories[repositoryIndex] = updatedRepository;
-
-  return response.json(updatedRepository);
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
@@ -71,24 +67,15 @@ app.delete("/repositories/:id", (request, response) => {
 app.post("/repositories/:id/like", (request, response) => {
   const { id } = request.params;
 
-  const repositoryIndex = repositories.findIndex(r => r.id === id);
+  const repository = repositories.find(r => r.id === id);
 
-  if (repositoryIndex < 0) {
+  if (!repository) {
     return response.status(400).json({ error: 'Repository not found.'})
   };
 
-  const currentRepository = repositories[repositoryIndex];
-  const { likes } = currentRepository;
-  addLikes = likes + 1
+  repository.likes += 1;
 
-  const updatedRepository = {
-    ...currentRepository,
-    likes: addLikes,
-  }
-
-  repositories[repositoryIndex] = updatedRepository;
-
-  return response.json(updatedRepository);
+  return response.json(repository);
 });
 
 module.exports = app;
